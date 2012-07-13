2e14d8cd9de9e46df034fd16f8730f16d1f1c9ab
konfetti <konfetti@gmx.net>
2012-07-13 17:54:49 +0200

    [ufs9xx_cic] rework module detection handling; add ufs913

--- cic/ufs9xx_cic/ufs9xx_cic.h
+++ cic/ufs9xx_cic/ufs9xx_cic.h
@@ -11,28 +11,39 @@ struct ufs9xx_cic_core {
         struct dvb_ca_en50221           ca; /* cimax */
 };
 
+#define SLOTSTATUS_NONE		1
+#define SLOTSTATUS_PRESENT	2
+#define SLOTSTATUS_RESET	4
+#define SLOTSTATUS_READY	8
+
 struct ufs9xx_cic_state {
-        struct dvb_frontend_ops                 ops;
-	struct ufs9xx_cic_core		        *core;
+    struct dvb_frontend_ops             ops;
+    struct ufs9xx_cic_core		        *core;
 
-#ifdef UFS922
-        struct i2c_adapter      		*i2c;
-        int					i2c_addr;
+#if defined(UFS922) || defined(UFS913)
+        struct i2c_adapter      	    *i2c;
+        int                             i2c_addr;
 #endif
 
-	struct stpio_pin			*slot_enable[cNumberSlots];
-	struct stpio_pin			*slot_reset[cNumberSlots];
-	struct stpio_pin			*slot_status[cNumberSlots];
+	struct stpio_pin                    *ci_reset;
+	struct stpio_pin                    *module_ready_pin[cNumberSlots];
 
-        int                                     module_ready[cNumberSlots];
-        int                                     module_present[cNumberSlots];
-        unsigned long                           detection_timeout[cNumberSlots];
+	struct stpio_pin                    *slot_enable[cNumberSlots];
+	struct stpio_pin                    *slot_reset[cNumberSlots];
+	struct stpio_pin                    *slot_status[cNumberSlots];
 
-#ifdef UFS922
-	/* which tuner to which ca? */
-	int					module_a_source;
-	int					module_b_source;
+    int                                 module_status[cNumberSlots];
+
+#if defined(UFS922) || defined(UFS913)
+	int					                module_source[cNumberSlots];
 #endif
+
+    volatile unsigned long              slot_attribute_read[cNumberSlots];
+    volatile unsigned long              slot_attribute_write[cNumberSlots];
+    volatile unsigned long              slot_control_read[cNumberSlots];
+    volatile unsigned long              slot_control_write[cNumberSlots];
+
+    unsigned long                       detection_timeout[cNumberSlots];
 };
 
 int init_ufs9xx_cic(struct dvb_adapter *dvb_adap);
