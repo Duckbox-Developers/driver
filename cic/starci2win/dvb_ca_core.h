8da6dfbecab4008d3fdb64238384616cc5c6c937
konfetti <konfetti@gmx.net>
2012-07-13 18:05:00 +0200

    [starci] rework module detection handling

--- cic/starci2win/dvb_ca_core.h
+++ cic/starci2win/dvb_ca_core.h
@@ -4,21 +4,23 @@
 #include "dvb_ca_en50221.h"
 #include "dvb_frontend.h"
 
+#define SLOTSTATUS_NONE		1
+#define SLOTSTATUS_PRESENT	2
+#define SLOTSTATUS_RESET	4
+#define SLOTSTATUS_READY	8
+
+#define cNumberSlots 2
+
 struct dvb_ca_state {
-	struct dvb_adapter			*dvb_adap;
-        struct dvb_frontend_ops                 ops;
-	struct dvb_ca_en50221			ca;
-        struct i2c_adapter      		*i2c;
-        int					i2c_addr;
-        int                                     module_ready[2];
-#if defined(ATEVIO7500) || defined(FORTIS_HDBOX) || defined(HS7810A) || defined(HS7110)
-        int                                     module_present[2];
-#endif
-        unsigned long                           detection_timeout[2];
+	struct dvb_adapter		*dvb_adap;
+	struct dvb_ca_en50221   ca;
+    struct i2c_adapter      *i2c;
+    int					    i2c_addr;
+
+    int                     module_status[cNumberSlots];
 };
 
 int init_cimax(struct dvb_adapter *dvb_adap);
-
 int setCiSource(int slot, int source);
 
 #endif
