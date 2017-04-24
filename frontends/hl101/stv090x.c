0fd0507805f11d29bc70bb2c61c34523870747b4
sid8796 <sid8796@gmail.com>
2017-04-24 13:00:41 +0300

    [HL101] stv090x
    include cleanup: Update gfp.h and slab.h includes to prepare for breaking implicit slab.h inclusion from percpu.h

--- frontends/hl101/stv090x.c
+++ frontends/hl101/stv090x.c
@@ -22,6 +22,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/string.h>
+#include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
