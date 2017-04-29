43432b4faecc9a50c64e1dcf81d7fbb7a1fb54eb
sid8796 <sid8796@gmail.com>
2017-04-29 17:28:49 +0300

    [hl101] stb0899: fix diseqc messages getting lost

--- frontends/hl101/stb0899_drv.c
+++ frontends/hl101/stb0899_drv.c
@@ -753,6 +753,7 @@ static int stb0899_send_diseqc_msg(struct dvb_frontend *fe, struct dvb_diseqc_ma
 	STB0899_SETFIELD_VAL(DISPRECHARGE, reg, 0);
 	stb0899_write_reg(state, STB0899_DISCNTRL1, reg);
 
+	msleep(100);
 	return 0;
 }
 
