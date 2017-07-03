e220a8e8fe393fe83c173581c7576d81cea2a6b2
alex.revetchi <alex.revetchi@gmail.com>
2017-07-03 19:57:45 +0200

    fix for possible null pointer dereference

--- frontends/spark_dvbapi5/ix7306.c
+++ frontends/spark_dvbapi5/ix7306.c
@@ -637,7 +637,7 @@ int ix7306_set_frequency(struct dvb_frontend *fe, u32 frequency)
 		frontend_ops = &fe->ops;
 	if (&frontend_ops->tuner_ops)
 		tuner_ops = &frontend_ops->tuner_ops;
-	if (tuner_ops->set_state)
+	if (tuner_ops && tuner_ops->set_state)
 	{
 		if ((err = tuner_ops->set_state(fe, DVBFE_TUNER_FREQUENCY, &t_state)) < 0)
 		{
