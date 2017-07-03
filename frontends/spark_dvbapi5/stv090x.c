3daf0f3cfcf27e2b8e1dfbab9e98675183252d47
CrazyCat <crazycat69@narod.ru>
2017-07-03 20:47:52 +0200

    Fix SNR calculation for stv090x

--- frontends/spark_dvbapi5/stv090x.c
+++ frontends/spark_dvbapi5/stv090x.c
@@ -4940,7 +4940,6 @@ static int stv090x_read_cnr(struct dvb_frontend *fe, u16 *cnr)
 	s32 val_0, val_1, val = 0;
 	u8 lock_f;
 	s32 snr;
-	s32 div;
 	switch (state->delsys)
 	{
 		case STV090x_DVBS2:
@@ -4960,9 +4959,9 @@ static int stv090x_read_cnr(struct dvb_frontend *fe, u16 *cnr)
 				}
 				val /= 4;
 				snr = stv090x_table_lookup(stv090x_s2cn_tab, ARRAY_SIZE(stv090x_s2cn_tab) - 1, val);
-				div = stv090x_s2cn_tab[0].read - stv090x_s2cn_tab[ARRAY_SIZE(stv090x_s2cn_tab) - 1].read;
-				*cnr = 0xFFFF - ((val * 0xFFFF) / div);
-				/* *cnr = snr * 0xFFFF / stv090x_s2cn_tab[ARRAY_SIZE(stv090x_s2cn_tab) - 1].real; */
+				if (snr < 0) snr = 0;
+				if (snr > 200) snr = 200;
+				*cnr = snr * 0xFFFF / 200;
 			}
 			break;
 		case STV090x_DVBS1:
@@ -4983,8 +4982,9 @@ static int stv090x_read_cnr(struct dvb_frontend *fe, u16 *cnr)
 				}
 				val /= 4;
 				snr = stv090x_table_lookup(stv090x_s1cn_tab, ARRAY_SIZE(stv090x_s1cn_tab) - 1, val);
-				div = stv090x_s1cn_tab[0].read - stv090x_s1cn_tab[ARRAY_SIZE(stv090x_s1cn_tab) - 1].read;
-				*cnr = 0xFFFF - ((val * 0xFFFF) / div);
+				if (snr < 0) snr = 0;
+				if (snr > 200) snr = 200;
+				*cnr = snr * 0xFFFF / 200;
 			}
 			break;
 		default:
