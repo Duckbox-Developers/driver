f45f41a1068a644cf0d786b8826a8186107a6416
sid8796 <sid8796@gmail.com>
2017-11-06 00:11:40 +0300

    [HL101]frontcontroller fix compilation

--- frontcontroller/proton/proton.c
+++ frontcontroller/proton/proton.c
@@ -216,7 +216,6 @@ static struct saved_data_s lastdata;
  * by a semaphore and the threads goes to sleep until
  * the answer has been received or a timeout occurs.
  */
-static char ioctl_data[BUFFERSIZE];
 
 int writePosition = 0;
 int readPosition = 0;
@@ -225,10 +224,7 @@ unsigned char receivedData[BUFFERSIZE];
 unsigned char str[64];
 static SegAddrVal_T VfdSegAddr[15];
 
-#define VFD_RW_SEM
-#ifdef VFD_RW_SEM
 struct rw_semaphore vfd_rws;
-#endif
 
 unsigned char ASCII[48][2] =
 {
@@ -320,9 +316,7 @@ unsigned char PROTONfp_RD(void)
 	int i;
 	unsigned char val = 0, data = 0;
 
-#ifdef VFD_RW_SEM
 	down_read(&vfd_rws);
-#endif
 
 	PROTONfp_Set_PIO_Mode(PIO_In);
 	for (i = 0; i < 8; i++)
@@ -342,9 +336,7 @@ unsigned char PROTONfp_RD(void)
 	udelay(1);
 	PROTONfp_Set_PIO_Mode(PIO_Out);
 
-#ifdef VFD_RW_SEM
 	up_read(&vfd_rws);
-#endif
 
 	return val;
 }
@@ -353,9 +345,7 @@ static int VFD_WR(unsigned char data)
 {
 	int i;
 
-#ifdef VFD_RW_SEM
 	down_write(&vfd_rws);
-#endif
 
 	for (i = 0; i < 8; i++)
 	{
@@ -372,9 +362,7 @@ static int VFD_WR(unsigned char data)
 		data >>= 1;
 	}
 
-#ifdef VFD_RW_SEM
 	up_write(&vfd_rws);
-#endif
 
 	return 0;
 }
@@ -536,13 +524,13 @@ void VFD_Draw_Num(unsigned char c, unsigned char position)
 	}
 }
 
-
 static int VFD_Show_Time(int hh, int mm)
 {
+	int res = 0;
 	if ((hh > 24) && (mm > 60))
 	{
 		dprintk(2, "%s bad parameter!\n", __func__);
-		return;
+		return res;
 	}
 
 	VFD_Draw_Num((hh / 10), 1);
@@ -553,7 +541,6 @@ static int VFD_Show_Time(int hh, int mm)
 
 	return 0;
 }
-
 
 static int VFD_Show_Ico(LogNum_T log_num, int log_stat)
 {
@@ -609,12 +596,10 @@ static int VFD_Show_Ico(LogNum_T log_num, int log_stat)
 	return 0 ;
 }
 
-
 static struct task_struct *thread;
 static int thread_stop  = 1;
 
-
-void clear_display()
+void clear_display(void)
 {
 	int j;
 	for (j = 0; j < 8; j++)
@@ -625,28 +610,25 @@ void clear_display()
 	VFD_Show_Content();
 }
 
-
 void draw_thread(void *arg)
 {
 	struct vfd_ioctl_data *data;
-	data = (struct vfd_ioctl_data *)arg;
-
 	struct vfd_ioctl_data draw_data;
+	int count = 0;
+	int pos = 0;
+	int k = 0;
+	int j = 0;
+	unsigned char c0;
+	unsigned char c1;
+	unsigned char temp;
+	unsigned char draw_buf[64][2];
+
+	data = (struct vfd_ioctl_data *)arg;
 
 	draw_data.length = data->length;
 	memcpy(draw_data.data, data->data, data->length);
 
 	thread_stop = 0;
-
-	unsigned char c = 0;
-	unsigned char c1 = 0;
-	unsigned char temp = 0;
-	unsigned char draw_buf[64][2];
-	int count = 0;
-	int pos = 0;
-
-	int k = 0;
-	int j = 0;
 
 	clear_display();
 
@@ -660,7 +642,7 @@ void draw_thread(void *arg)
 		}
 
 
-		c = c1 = temp = 0;
+		c0 = c1 = temp = 0;
 
 		if (draw_data.data[pos] == 32)
 		{
@@ -693,7 +675,7 @@ void draw_thread(void *arg)
 				temp = 47;
 			if (temp < 48)
 			{
-				c  = ASCII[temp][0];
+				c0 = ASCII[temp][0];
 				c1 = ASCII[temp][1];
 			}
 		}
@@ -703,27 +685,27 @@ void draw_thread(void *arg)
 			switch (draw_data.data[pos - 1])
 			{
 				case 0xc2:
-					c  = UTF_C2[draw_data.data[pos] & 0x3f][0];
+					c0 = UTF_C2[draw_data.data[pos] & 0x3f][0];
 					c1 = UTF_C2[draw_data.data[pos] & 0x3f][1];
 					break;
 				case 0xc3:
-					c  = UTF_C3[draw_data.data[pos] & 0x3f][0];
+					c0 = UTF_C3[draw_data.data[pos] & 0x3f][0];
 					c1 = UTF_C3[draw_data.data[pos] & 0x3f][1];
 					break;
 				case 0xc4:
-					c  = UTF_C4[draw_data.data[pos] & 0x3f][0];
+					c0 = UTF_C4[draw_data.data[pos] & 0x3f][0];
 					c1 = UTF_C4[draw_data.data[pos] & 0x3f][1];
 					break;
 				case 0xc5:
-					c  = UTF_C5[draw_data.data[pos] & 0x3f][0];
+					c0 = UTF_C5[draw_data.data[pos] & 0x3f][0];
 					c1 = UTF_C5[draw_data.data[pos] & 0x3f][1];
 					break;
 				case 0xd0:
-					c  = UTF_D0[draw_data.data[pos] & 0x3f][0];
+					c0 = UTF_D0[draw_data.data[pos] & 0x3f][0];
 					c1 = UTF_D0[draw_data.data[pos] & 0x3f][1];
 					break;
 				case 0xd1:
-					c  = UTF_D1[draw_data.data[pos] & 0x3f][0];
+					c0 = UTF_D1[draw_data.data[pos] & 0x3f][0];
 					c1 = UTF_D1[draw_data.data[pos] & 0x3f][1];
 					break;
 			}
@@ -741,14 +723,14 @@ void draw_thread(void *arg)
 		}
 
 
-		draw_buf[count][0] = c;
+		draw_buf[count][0] = c0;
 		draw_buf[count][1] = c1;
 		count++;
 
 		pos++;
 	}
 
-#if ENABLE_SCROLL // j00zek: disabled by default, interfere with GUI's
+#ifdef ENABLE_SCROLL // j00zek: disabled by default, interfere with GUI's
 	if (count > 8)
 	{
 		pos  = 0;
@@ -782,6 +764,11 @@ void draw_thread(void *arg)
 		k = 8;
 		if (count < 8)
 			k = count;
+		if(kthread_should_stop())
+		{
+			thread_stop = 1;
+			return;
+		}
 		clear_display();
 
 		for (j = 0; j < k; j++)
@@ -792,6 +779,8 @@ void draw_thread(void *arg)
 		VFD_Show_Content();
 	}
 
+	if(count == 0)
+		clear_display();
 	thread_stop = 1;
 }
 
@@ -809,7 +798,7 @@ int run_draw_thread(struct vfd_ioctl_data *draw_data)
 
 
 	thread_stop = 2;
-	thread = kthread_run(draw_thread, draw_data, "draw_thread", NULL, true);
+	thread = kthread_run(draw_thread, draw_data, "draw_thread");
 
 	//wait thread run
 	while (thread_stop == 2)
@@ -819,8 +808,6 @@ int run_draw_thread(struct vfd_ioctl_data *draw_data)
 
 	return 0;
 }
-
-
 
 static int VFD_Show_Time_Off(void)
 {
@@ -954,9 +941,7 @@ int vfd_init_func(void)
 		return -1;
 	}
 
-#ifdef VFD_RW_SEM
 	init_rwsem(&vfd_rws);
-#endif
 
 	VFD_CS_CLR();
 	VFD_WR(0x0C);
@@ -970,7 +955,7 @@ int vfd_init_func(void)
 	return 0;
 }
 
-static void VFD_CLR()
+static void VFD_CLR(void)
 {
 	VFD_CS_CLR();
 	VFD_WR(0x0C);
@@ -984,8 +969,7 @@ static void VFD_CLR()
 
 int protonSetIcon(int which, int on)
 {
-	char buffer[8];
-	int  res = 0, m = 0, n = 0;
+	int  res = 0;
 
 	dprintk(5, "%s > %d, %d\n", __func__, which, on);
 	if (which < 1 || which > 45)
@@ -1307,8 +1291,8 @@ static struct file_operations vfd_fops =
 {
 	.owner = THIS_MODULE,
 	.ioctl = PROTONdev_ioctl,
-	.write = PROTONdev_write,
-	.read  = PROTONdev_read,
+	.write = (void *) PROTONdev_write,
+	.read  = (void *) PROTONdev_read,
 	.poll  = (void *) PROTONdev_poll,
 	.open  = PROTONdev_open,
 	.release  = PROTONdev_close
